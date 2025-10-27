288b5ed7288b60aa17b1bb7d7381de0049731133
Ivan Jesus Lopez <Ivanjl849@gmail.com>
2025-10-27 16:46:28 +0100

    navbar dinamico

--- script.js
+++ script.js
@@ -6,9 +6,24 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
+let lastScrollPosition = 0;
 const formacion = document.getElementById("formacion");
 const aptitudes = document.getElementById("aptitudes");
 const proyectos = document.getElementById("proyectos");
+const navbar = document.getElementById("mainNavbar");
+
+window.addEventListener("scroll", () => {
+    //si aumenta el numero el menu nav se esconde y si disminuye se muestra
+    if (window.scrollY > lastScrollPosition) {
+        navbar.classList.add("navbarVisible");
+        lastScrollPosition = window.scrollY;
+        console.log("ESTA BAJANDO");
+    } else {
+        navbar.classList.remove("navbarVisible");
+        console.log("ESTA SUBIENDO");
+    }
+});
+
 
 observer.observe(formacion);
 observer.observe(aptitudes);
